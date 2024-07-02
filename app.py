209c30fc6b31e5db4c465b0d24aa58646eb62d7d
Ranjan <153456933+vvXranjan@users.noreply.github.com>
2024-07-02 14:13:35 +0000

    added company_name

--- app.py
+++ app.py
@@ -3,10 +3,35 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [{
+  'id ': 1,
+  'title': 'Data Analyst',
+  'location': 'Bengaluru, India',
+  'salary': 'Rs. 1,00,000'
+},
+{
+  'id ': 2,
+  'title': 'Backend Engineer',
+  'location': 'Delhi, India',
+  'salary': 'Rs. 15,00,000'
+},
+{
+  'id ': 3,
+  'title': 'Data Analyst',
+  'location': 'Chennai, India',
+  'salary': 'Rs. 12,00,000'
+},{
+  'id ': 4,
+  'title':'Frontend Engineer',
+  'location': 'Remote',
+  'salary': 'Rs. $150,000'
+},
+]
+
 
 @app.route('/')
 def hello_jovian():
-  return render_template('home.html')
+  return render_template('home.html',jobs=JOBS,company_name="Jovian")
 
 
 if __name__ == "__main__":
