a600ecef4c2aabb72bd89720e0de00c630cf46e0
Ranjan <153456933+vvXranjan@users.noreply.github.com>
2024-07-02 04:21:20 +0000

    push

---
+++ app.py
@@ -0,0 +1,13 @@
+from re import DEBUG
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+  return 'Hello, vvr!'
+
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', port=8080, debug=True)
