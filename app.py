65fdebfbaf001f4a074c4106e43777d6be72747b
Ranjan <153456933+vvXranjan@users.noreply.github.com>
2024-07-03 04:36:19 +0000

    added dynamic data and api route

--- app.py
+++ app.py
@@ -34,5 +34,10 @@ def hello_jovian():
   return render_template('home.html',jobs=JOBS,company_name="Jovian")
 
 
+@app.route('/api/jobs')
+def list_jobs():
+  return jsonify(JOBS)
+  
+
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=8080, debug=True)
